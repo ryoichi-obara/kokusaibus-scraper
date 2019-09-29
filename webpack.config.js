61eb6a76312881124b8ce9100ba3cc730e4e61b8
Ryoichi Obara <ryoichi-obara@users.noreply.github.com>
2019-09-30 00:13:12 +0900

    Individual packages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const glob = require('glob');
+const slsw = require('serverless-webpack');
+// const glob = require('glob');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
@@ -10,9 +11,12 @@ module.exports = {
   target: 'async-node',
   devtool: 'source-map',
   externals: [nodeExternals()],
-  entry: glob.sync('./src/**/*.js'),
-  output: {
-    path: `${__dirname}/dist`,
-    filename: 'index.js',
-  },
+  // Serverlessのpackage.individuallyを使用する場合にはentryをserverless-webpackに任せる必要がある(slsw.lib.entries)
+  // 基本的に問題は起こらないと思うが、entryが足りない！みたいなケースには下記の様なコードで対応しろとのこと(_はlodashなので適宜導入下さい)
+  // entry: glob.sync('./src/**/*.js'),
+  entry: slsw.lib.entries,
+  // output: {
+  //   path: `${__dirname}/dist`,
+  //   filename: 'index.js',
+  // },
 };
